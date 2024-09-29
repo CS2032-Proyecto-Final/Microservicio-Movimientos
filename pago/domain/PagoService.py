f0732d9f34bb4e2f6a88c8ef5ce919ade7e9d5d7
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-09-29 02:47:29 -0500

    ya hace los fetchs y deberia funcionar todo.

--- pago/domain/PagoService.py
+++ pago/domain/PagoService.py
@@ -1,9 +1,13 @@
 from typing import List
+
+from fastapi import HTTPException
+
 from pago.domain.Pago import Pago
 from pago.dto.NewPagoDto import NewPagoDto
 from pago.dto.PagoResponseDto import PagoResponseDto
 from pago.infrastructure.PagoRepository import PagoRepository
 from datetime import datetime
+import requests
 
 
 class PagoService:
@@ -23,12 +27,39 @@ class PagoService:
 
     def getPagosByClienteId(self, id: int) -> List[PagoResponseDto]:
         fullLista = self.repo.findAllByClienteId(id)
+
+        tienda_ids = [i.destinatario_id for i in fullLista]
+        producto_ids = [i.producto_id for i in fullLista]
+
+        tienda_response = requests.post(
+            "http://api-clientes/tiendas/nombre",
+            json={tienda_ids}
+        )
+
+        producto_response = requests.post(
+            "http://api-promociones/productos/nombre",
+            json={producto_ids}
+        )
+
+        if tienda_response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Error al comunicarse con el microservicio de clientes")
+
+        if producto_response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Error al comunicarse con el microservicio de promociones")
+
+        nombre_tiendas = {item["tienda_id"]: item["nombre_tienda"] for item in tienda_response.json()}
+        nombre_productos = {item["producto_id"]: item["nombre_producto"] for item in producto_response.json()}
+
         dataLista = []
 
         for i in fullLista:
-            pagoData = PagoResponseDto(destinatario_nombre=str(i.destinatario_id),
+
+            nombre_tienda = nombre_tiendas.get(i.remitente_id, "")
+            nombre_producto = nombre_productos.get(i.producto_id, "")
+
+            pagoData = PagoResponseDto(destinatario_nombre=nombre_tienda,
                                        monto=i.monto,
-                                       producto_nombre=str(i.producto_id),
+                                       producto_nombre=nombre_producto,
                                        fecha=i.fecha,
                                        codigo=i.codigo)
             dataLista.append(pagoData)
--- transferencia/domain/TransferenciaService.py
+++ transferencia/domain/TransferenciaService.py
@@ -1,10 +1,14 @@
 from typing import List
+
+from fastapi import HTTPException
+
 from transferencia.domain.Transferencia import Transferencia
 from transferencia.dto.TransferenciaRemitenteResponseDto import TransferenciaRemitenteResponseDto
 from transferencia.dto.TransferenciaDestinatarioResponseDto import TransferenciaDestinatarioResponseDto
 from transferencia.dto.NewTransferenciaDto import NewTransferenciaDto
 from transferencia.infrastructure.TransferenciaRepository import TransferenciaRepository
 from datetime import datetime
+import requests
 
 
 class TransferenciaService:
@@ -13,10 +17,26 @@ class TransferenciaService:
 
     def getDestinatariosByRemitenteId(self, id: int) -> List[TransferenciaDestinatarioResponseDto]:
         fullLista = self.repo.findAllByRemitenteId(id)
+
+        destinatario_ids = [i.destinatario_id for i in fullLista]
+
+        response = requests.post(
+            "http://api-clientes/personas/nombre",
+            json={destinatario_ids}
+        )
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Error al comunicarse con el microservicio de clientes")
+
+        nombres_destinatarios = {item["id"]: item["nombre_destinatario"] for item in response.json()}
+
         dataLista = []
 
         for i in fullLista:
-            transferenciaData = TransferenciaDestinatarioResponseDto(nombre_destinatario=str(i.destinatario_id),
+
+            nombre_destinatario = nombres_destinatarios.get(i.destinatario_id, "")
+
+            transferenciaData = TransferenciaDestinatarioResponseDto(nombre_destinatario=nombre_destinatario,
                                                                      monto=i.monto,
                                                                      fecha=i.fecha,
                                                                      descripcion=i.descripcion)
@@ -26,10 +46,25 @@ class TransferenciaService:
 
     def getRemitentesByDestinatarioId(self, id: int) -> List[TransferenciaRemitenteResponseDto]:
         fullLista = self.repo.findAllByDestinatarioId(id)
+
+        remitente_ids = [i.remitente_id for i in fullLista]
+
+        response = requests.post(
+            "http://api-clientes/personas/nombre",
+            json={remitente_ids}
+        )
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=500, detail="Error al comunicarse con el microservicio de clientes")
+
+        nombres_remitentes = {item["id"]: item["nombre_destinatario"] for item in response.json()}
+
         dataLista = []
 
         for i in fullLista:
-            transferenciaData = TransferenciaRemitenteResponseDto(nombre_remitente=str(i.remitente_id),
+            nombre_remitente = nombres_remitentes.get(i.remitente_id, "")
+
+            transferenciaData = TransferenciaRemitenteResponseDto(nombre_remitente=nombre_remitente,
                                                                   monto=i.monto,
                                                                   fecha=i.fecha,
                                                                   descripcion=i.descripcion)
